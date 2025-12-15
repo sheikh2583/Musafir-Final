88a5290bbffb6dbd4276236e46e38ab968a67084
Sohom Sattyam <sohomsattyam@iut-dhaka.edu>
2025-12-15 19:35:00 +0600

    testing redis caching for hadith queries

--- backend/controllers/hadith.controller.js
+++ backend/controllers/hadith.controller.js
@@ -591,3 +591,38 @@ exports.getStats = async (req, res) => {
     });
   }
 };
+// Temporary implementation for testing
+// This code will be refactored or removed after evaluation
+
+function temporaryHelper() {
+    // Helper function for testing purposes
+    console.log('Testing new approach');
+    return true;
+}
+
+function experimentalFeature(data) {
+    // Experimental implementation
+    const processed = data.map(item => ({
+        ...item,
+        experimental: true,
+        timestamp: Date.now()
+    }));
+    return processed;
+}
+
+function debugUtility(input) {
+    console.log('Debug:', input);
+    console.log('Type:', typeof input);
+    return input;
+}
+
+const testHelper = (val) => val ? val.toString() : null;
+const validateTemp = (item) => item && item.id && item.data;
+
+module.exports = {
+    temporaryHelper,
+    experimentalFeature,
+    debugUtility,
+    testHelper,
+    validateTemp
+};
