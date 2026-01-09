08ae0c6407e8219deace86130fa1a2c31b9d936b
Mahiul Kabir <mahiulkabir@outlook.com>
2026-01-09 21:24:00 +0600

    refactor: simplified ayah processing, removed helpers

--- backend/controllers/quran.controller.js
+++ backend/controllers/quran.controller.js
@@ -1,4 +1,4 @@
-const fs = require('fs');
+﻿const fs = require('fs');
 const path = require('path');
 const { getSearchEngine } = require('../ml-search/search/vector-search');
 
@@ -558,38 +558,4 @@ exports.semanticSearch = async (req, res) => {
     });
   }
 };
-// Temporary implementation for testing
-// This code will be refactored or removed after evaluation
 
-function temporaryHelper() {
-    // Helper function for testing purposes
-    console.log('Testing new approach');
-    return true;
-}
-
-function experimentalFeature(data) {
-    // Experimental implementation
-    const processed = data.map(item => ({
-        ...item,
-        experimental: true,
-        timestamp: Date.now()
-    }));
-    return processed;
-}
-
-function debugUtility(input) {
-    console.log('Debug:', input);
-    console.log('Type:', typeof input);
-    return input;
-}
-
-const testHelper = (val) => val ? val.toString() : null;
-const validateTemp = (item) => item && item.id && item.data;
-
-module.exports = {
-    temporaryHelper,
-    experimentalFeature,
-    debugUtility,
-    testHelper,
-    validateTemp
-};
